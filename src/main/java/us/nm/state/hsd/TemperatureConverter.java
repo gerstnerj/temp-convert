74a480dba93e258f412cbb9a0fe3aa52201af2da
gerstnerj <gerstnerj@ITDSFI-G022421X>
2021-03-01 16:25:05 -0700

    First commit, test if successfully created git repository.

---
+++ src/main/java/us/nm/state/hsd/TemperatureConverter.java
@@ -0,0 +1,15 @@
+package us.nm.state.hsd;
+
+public class TemperatureConverter {
+
+    public static double celsiusToFahrenheit(double celsius) {
+/*	//double fahrenheit = 0;
+	double fahrenheit = celsius * 9 / 5 + 32;
+	return fahrenheit;
+*/	
+	return celsius * 9 / 5 + 32;
+    }
+    
+    
+    
+}
